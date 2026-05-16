103af2440aed2d3ffe507770011a89e3ee2a5563
Bladimir Jose Villarroel Guevara <bladimir.dev.contato@gmail.com>
2026-05-15 21:44:10 -0300

    Postei o "inicio" da atividade 1

---
+++ aula8/atividade8-1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Aluno{
+	private:
+		string nome;
+		int num;
+		int idade;
+	public:
+		//construtores
+		Aluno(){
+			
+		};
+		//setters
+		void setNome(string nome){
+			
+		}
+		void setNum(int num){
+			
+		}
+		void setIdade(int idade){
+			
+		}
+		//getters
+		string getNome(){
+			return //;
+		};
+		int getNum(){
+			return //;
+		}
+		int getIdade(){
+			return //;
+		};
+		//showers
+};
+
+class Turma{
+	private:
+		string section;
+		string sala;
+		Aluno* alunos[];
+	public:
+		//costrutores
+		Turma (){
+			
+		}
+		//setters
+		void setSection(){
+			
+		}
+		void setSala(){
+			
+		}
+		void setAlunos(){
+			
+		}
+		//getters
+		string getSection(){
+			return //;
+		}
+		string getSala(){
+			return //;
+		}
+		/*?? getAlunos(){
+			return //;
+		}*/
+		
+		//showers
+		void showAlunos(){
+			
+		}
+};
+
+int main (){
+	
+	return 0;
+}
